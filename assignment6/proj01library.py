df634cabf83a3cc7bc008b274375f1645ec9fc11
EricRootLee <erickiokom@gmail.com>
2021-01-26 19:25:51 +0300

    Done with assignment 1

---
+++ assignment6/proj01library.py
@@ -0,0 +1,62 @@
+ASCII_LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+ASCII_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DECIMAL_DIGITS = "0123456789"
+# Added
+ALPHA_NUMERICS = ASCII_LOWERCASE + ASCII_UPPERCASE
+
+
+def is_alpha(string: str):
+    for letters in string:
+        if letters not in ALPHA_NUMERICS:
+            return False
+    return True
+
+
+def is_digit(string: str):
+    for letters in string:
+        if letters not in DECIMAL_DIGITS:
+            return False
+    return True
+
+
+def to_lower(string: str):
+    for letters in string:
+        if letters not in ASCII_LOWERCASE:
+            return string.lower()
+    return string
+
+
+def to_upper(string: str):
+    for letters in string:
+        if letters not in ASCII_UPPERCASE:
+            return string.upper()
+    return string
+
+
+def find_chr(string1: str, string2: str):
+    if len(string2) == 1:
+        if string2 in string1:
+            return string1.index(string2)
+    return -1
+
+
+def find_str(string1: str, string2: str):
+    if string2 in string1:
+        return string1.index(string2)
+    return -1
+
+
+def replace_chr(string1: str, string2: str, string3: str):
+    if len(string2) == 1 and len(string3) == 1:
+        if string2 in string1:
+            return string1.replace(string2, string3)
+    return " "
+
+
+def replace_str(string1: str, string2: str, string3: str):
+    if string2 in string1:
+        return string1.replace(string2, string3)
+    elif len(string2.strip()) == 0:
+        return f'{string3}{string3.join(string1[i:i + 1] for i in range(0, len(string1), 1))}{string3}'
+    elif string2 not in string1:
+        return string1
---
+++ assignment6/proj01test.py
@@ -0,0 +1,44 @@
+from proj01library import *
+
+if __name__ == '__main__':
+    # To Lower
+    print('TO LOWER TEST')
+    print(f'Test To Lower Case : (Python) to LowerCase ? {to_lower("Python")}')
+    # To Upper
+    print('TO UPPER TEST')
+    print(f'Test To Upper Case : (pythOon) to Upper Case ? {to_upper("pythOon")}')
+    # Is Digit Test
+    print('IS DIGIT TEST')
+    print(f'Test s Digit : (pyt3hon) is in Upper Case ? {is_digit("pyt3hon")}')
+    print(f'Test s Digit : (python) is in Upper Case ? {is_digit("python")}')
+    print(f'Test s Digit : (12345) is in Upper Case ? {is_digit("12345")}')
+    # Is Alpha Test
+    print('IS ALPHA TEST')
+    print(f'Test is Alpha : (pyt3hon) is alpha ? {is_alpha("pyt3hon")}')
+    print(f'Test is Alpha : (pythonLang) is alpha ? {is_alpha("pythonLang")}')
+    print(f'Test is Alpha : (Pythonlang) is alpha ? {is_alpha("Pythonlang")}')
+
+    # Find Char Test
+    print('Find Char TEST')
+    print(f'Test Find Char Index : n in (pyt3hon)? {find_chr("pyt3hon", "n")}')
+    print(f'Test Find Char Index : ns in (pyt3hon) ? {find_chr("pyt3hon", "ns")}')
+    print(f'Test Find Char Index :  in (pyt3hon) ? {find_chr("pyt3hon", "")}')
+    print(f'Test Find Char Index : r in (pyt3hon) ? {find_chr("pyt3hon", "r")}')
+
+    # Find Str Test
+    print('Find Char TEST')
+    print(f'Test Find Str Index : n in (pyt3hon)? {find_str("pyt3hon lang", "lang")}')
+    print(f'Test Find Str Index : ns in (pyt3hon) ? {find_str("pyt3hon", "ns")}')
+    print(f'Test Find Str Index : r in (pyt3hon) ? {find_str("pyt3hon", "r")}')
+
+    # Replace Char Test
+    print('Replace Char TEST')
+    print(f'Replace Char : 3 with 2 in (pyt3hon)? {replace_chr("pyt3hon", "3", "2")}')
+    print(f'Replace Char : ns in (pyt3hon) ? {replace_chr("pyt3hon", "ns","df")}')
+
+    # Replace Str Test
+    print('Replace Str TEST')
+    print(f'Replace Str : lang with lugha in (pyt3hon)? {replace_str("pyt3hon", "3", "2")}')
+    print(f'Replace Str : ns in (pyt3hon) ? {replace_str("pyt3hon", "ns","df")}')
+    print(f'Replace Str : " "  in (pyt3hon) ? {replace_str("pyt3hon", " ", "df")}')
+
