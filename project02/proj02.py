13d7866f2053f8c0003285bdf1be7ad8c4065855
EricRootLee <erickiokom@gmail.com>
2021-01-26 20:26:25 +0300

    Reading text

--- project02/proj02.py
+++ project02/proj02.py
@@ -1,5 +1,8 @@
+
 if __name__ == '__main__':
-def get_data_list(FILE_NAME):
-    f = open(FILE_NAME, "r")
-    print(f.read())
-get
+    def get_data_list(FILE_NAME):
+        f = open(FILE_NAME, "r")
+        print(f.read().split(" "))
+
+
+    get_data_list('table.csv')
---
+++ project02/template.py
@@ -0,0 +1,28 @@
+
+#name: get_data_list
+#param: FILE_NAME <str> - the file's name you saved for the stock's prices
+#brief: get a list of the stock's records' lists
+#return: a list of lists <list>
+def get_data_list(FILE_NAME):
+    return
+
+#name: get_monthly_averages
+#param: data_list <list> - the list that you will process
+#brief: get a list of the stock's monthly averages and their corresponding dates
+#return: a list <list>
+def get_monthly_averages(data_list):
+    return   
+
+#name: print_info
+#param: monthly_average_list <list> - the list that you will process
+#brief: print the top 6 and bottom 6 months for Google stock
+#return: None
+def print_info(monthly_average_list):
+    return
+
+# call get_data_list function to get the data list, save the return in data_list
+
+# call get_monthly_averages function with the data_list from above, save the 
+# return in monthly_average_list
+
+# call print_info function with the monthly_average_list from above 
